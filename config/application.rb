9c93f19618d9ff716630b0aed1b6f765ab7090b9
min90 <sorensen_hnd90@hotmail.com>
2017-04-21 14:21:24 +0200

    css

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,10 @@ module IOT
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.time_zone = 'Copenhagen'
-
+    config.cache_classes = true
+    config.serve_static_assets = true
+    config.assets.compile = true
+    config.assets.digest = true
 
   end
 end
